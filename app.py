67431802d49c944eab3ed91e3cc797f5d3728157
Bruce Nguyen <bruce.nguyen@rocsole.com>
2022-05-24 17:57:36 +0300

    config Kafka

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 import dash, json
 import plotly.express as px, pandas as pd, numpy as np
-import plotly.graph_objects as go
+import plotly.graph_objects as go, plotly.io as pio
 from dash import dcc, html
 from dash.dependencies import Input, Output
 from kafka import KafkaConsumer
 
-app = dash.Dash(__name__)
-app.layout = html.Div(
-    html.Div([
-        html.H1('PIG Demonstration'),
-        dcc.Graph(id='live-update-graph'),
-        dcc.Interval(
-            id='interval-component',
-            interval=1*1000,
-            n_intervals=0
-        )
-    ])
-)
+
+def get_layout():
+    return html.Div(
+        html.Div([
+            html.H1('PIG'),
+            dcc.Graph(id='live-update-graph'),
+            dcc.Interval(
+                id='interval-component',
+                interval=1 * 1000,
+                n_intervals=0
+            )
+        ])
+    )
+
+
+app = dash.Dash(__name__, update_title=None)
+app.layout = get_layout
 
 
 @app.callback(Output('live-update-graph', 'figure'),
@@ -24,8 +29,17 @@ app.layout = html.Div(
 def function_square(n):
     df_plot = pd.read_csv('df_to_plot.csv', index_col=False)
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df_plot.idx, y=df_plot.target, name="target", line_shape='spline'))
-    fig.add_trace(go.Scatter(x=df_plot.idx, y=df_plot.predict, name="predict", line_shape='spline'))
+    fig.add_trace(go.Scatter(x=df_plot.idx,
+                             y=df_plot.target,
+                             name="target",
+                             line_shape='spline',
+                             ))
+    fig.add_trace(go.Scatter(x=df_plot.idx,
+                             y=df_plot.predict,
+                             name="predict",
+                             line_shape='spline',
+                             fill='tonexty'))
+    fig.update_layout(template='plotly_dark')
     return fig
 
 
--- push_data.py
+++ push_data.py
@@ -1,16 +1,16 @@
-import pandas as pd, uuid, json, datetime, time
+import pandas as pd, uuid, json, datetime, time, argparse
 from kafka import KafkaProducer
 from train import get_data
 
 
 def producing():
-    train_inputs, train_cls_label, train_deposit_thickness, train_inner_diameter, _, _ = get_data('train_new.h5',
+    train_inputs, train_cls_label, train_deposit_thickness, train_inner_diameter, _, _ = get_data('val_new.h5',
                                                                                                   False,
                                                                                                   True)
     df = pd.DataFrame({'inputs': train_inputs.tolist(), 'labels': train_deposit_thickness.flatten()})
 
-    producer = KafkaProducer(bootstrap_servers='10.8.8.105:9092')
-    for i in range(100):
+    producer = KafkaProducer(bootstrap_servers=KAFKA_HOST)
+    for i in range(1000):
         mess_id = str(uuid.uuid4())
         date_time = datetime.datetime.now()
         mess = {
@@ -31,4 +31,9 @@ def producing():
 
 
 if __name__ == '__main__':
+    model_parser = argparse.ArgumentParser()
+    model_parser.add_argument('--host', default='localhost:9092', type=str, help='Kafka Host')
+    args = model_parser.parse_args()
+
+    KAFKA_HOST = args.host
     producing()
